cd9627f3b94cf366627934728339bd00e131e616
Rene Welch <welch16@gmail.com>
2019-08-16 16:07:26 -0500

    added license and shared functions

---
+++ R/shared_functions.R
@@ -0,0 +1,42 @@
+##' @import magrittr
+NULL
+
+
+##' converts from input files into the filtered file format for dada2 pipeline
+##'
+##' @param fastq a character vector with the name of a file
+##' @param outdir a character with the name of the output directory
+##'
+##' @return a character vector with modified
+##'
+##' @example
+##' parse_filtered_file("test.fastq","./outdir")
+parse_filtered_file <- function(fastq,outdir){
+
+  file.path(
+    outdir,
+    str_replace(basename(fastq), ".fastq", "_filtered.fastq.gz")
+  )
+
+}
+
+
+##' write csv files structured to make the dada2 pipeline
+##' run with condor
+##'
+##' @param all_samples a \code{tibble} with 3 columns:
+##'    name, R1, R2
+##' @param outfile the file where the full tibble is going to be saved,
+##'   the R1 and R2 files, are going to share almost the same name
+##'   but replacing `.csv` with `_R1.csv` and `_R2.csv`, respectively.
+##'
+save_files <- function(all_samples, outfile )
+{
+
+  all_samples %>% write_csv( outfile, col_names = FALSE)
+  all_samples %>% select(R1) %>%
+    write_csv(str_replace(outfile,".csv","_R1.csv"), col_names = FALSE)
+  all_samples %>% select(R2) %>%
+    write_csv(str_replace(outfile,".csv","_R2.csv"), col_names = FALSE)
+
+}
