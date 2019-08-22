d8135578a58545fa681d503b869eed26b1e48b4b
Rene Welch <welch16@gmail.com>
2019-08-22 12:52:08 -0500

    finished first qc part

--- R/quality_control_diagnostics.R
+++ R/quality_control_diagnostics.R
@@ -1,6 +1,10 @@
 ##' @import furrr
 ##' @import future
+##' @import ggplot2
 ##' @importFrom tidyr unnest
+##' @importFrom tidyr gather
+##' @importFrom forcats fct_relevel
+##' @importFrom stats median
 NULL
 
 ##' summarizes the number of reads resulted after each step into one table
@@ -56,3 +60,67 @@ summarize_number_reads <- function(reads_file,outprefix,outdir,cores)
       perc.asv_table = reads.asv_table / reads.in  )
 
 }
+
+
+##' compares the (relative) abundance for each step
+##' @param nreads_summary output of the \code{summarize_number_reads} function
+##' @param summary_fun function used to summarize the trend, the default function is the \code{median}
+##' @param relative boolean indicator determining wheter comparing the relative abundance
+##' @return a \code{ggplot} object
+##' @export
+plot_abundance_per_step <- function(nreads_summary, summary_fun = median, relative = FALSE)
+{
+  name <- step <- sample <- NULL
+  if(relative){
+
+    nreads_summary <- select(nreads_summary,name,sample,contains("perc"))
+    nreads_summary <- mutate(nreads_summary,perc.in = 1)
+
+  }else{
+
+    nreads_summary <- select(nreads_summary,name,sample,contains("reads"))
+
+  }
+
+  nreads_summary <- tidyr::gather(nreads_summary,step,value,-name,-sample)
+  nreads_summary <- mutate(nreads_summary,
+      step = stringr::str_remove(step,"reads."),
+      step = stringr::str_remove(step,"perc."),
+      step = factor(step) %>% fct_relevel("in","out","merged_pairs"))
+
+  fun_summary <- group_by(nreads_summary,step)
+  fun_summary <- summarize(fun_summary, value = summary_fun(value))
+
+  base_plot <- nreads_summary %>%
+    ggplot(aes(step,value))+
+    geom_boxplot()+
+    geom_point(alpha = 1/4,shape = 21)+
+    geom_line(aes(group = name),alpha = 1/4)+
+    geom_line(data = fun_summary,size = 1.5,aes(group = 1), linetype = 2,
+              colour = "red")+
+    theme(
+      legend.position = "none",
+      strip.text.y = element_text(angle = -90, size =10),
+      strip.background = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_blank()
+    )+
+    labs(
+      x = "processing step",
+      y = str_c(if_else(relative,"relative",""), "abundance after step",sep =" "))
+
+  if(relative){
+    base_plot +
+      scale_y_continuous(labels = scales::percent_format())
+  }else{
+    base_plot +
+      scale_y_continuous(labels = scales::comma_format(scale = 1e-3,suffix = "K"))
+  }
+
+
+
+
+}
+
+
+
