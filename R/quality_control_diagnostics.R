a7ae714e34b346e4d892274620cc7ab7948a5fc8
Rene Welch <welch16@gmail.com>
2020-02-06 14:48:41 -0600

    fixed some lintr issues in functions

--- R/quality_control_diagnostics.R
+++ R/quality_control_diagnostics.R
@@ -1,40 +1,43 @@
-##' @import furrr
-##' @import future
-##' @import ggplot2
-##' @importFrom tidyr unnest
-##' @importFrom tidyr gather
-##' @importFrom forcats fct_relevel
-##' @importFrom stats median
+#' @import future
+#' @import furrr
+#' @import ggplot2
+#' @importFrom tidyr unnest
+#' @importFrom tidyr gather
+#' @importFrom forcats fct_relevel
+#' @importFrom stats median
 NULL
 
-##' summarizes the number of reads resulted after each step into one table
-##'
-##' @param reads_file, a 3 column csv file with columns: sample | R1.fastq | R2.fastq
-##' @param outprefix prefix used in the final ASV table
-##' @param outdir names of the output directory where the results are saved
-##' @param cores number of cpus used to run the process
-##' @export
-summarize_number_reads <- function(reads_file,outprefix,outdir,cores)
-{
-  `.` <- R1 <- R2 <- NULL
-  name <- fold_change <- reads.out <- reads.in <- reads.merged_pairs <- NULL
+#' summarizes the number of reads resulted after each step into one table
+#'
+#' @param reads_file, a 3 column csv file with columns:
+#'   sample | R1.fastq | R2.fastq
+#' @param outprefix prefix used in the final ASV table
+#' @param outdir names of the output directory where the results are saved
+#' @param cores number of cpus used to run the process
+#' @export
+summarize_number_reads <- function(reads_file, outprefix, outdir, cores) {
+  `.` <- end1 <- end2 <- NULL
+  name <- fold_change <- reads.out <- NULL
+  reads.in <- reads.merged_pairs <- NULL
   reads.asv_table <- NULL
 
-  future::plan(future::multiprocess,workers = cores)
+  future::plan(future::multiprocess, workers = cores)
 
   stopifnot(
     file.exists(reads_file),
     dir.exists(outdir))
 
   ### read input file, i.e. the 3 column file
-  input_files = read_csv(reads_file,col_names = FALSE) %>% set_names(c("name","R1","R2"))
+  input_files <- read_csv(reads_file, col_names = FALSE) %>%
+    set_names(c("name", "end1", "end2"))
 
   input_files %<>%
-    mutate(
-      summ = file.path(outdir,"filter_fastq_summary",paste0(name,"_trim_summary.rds")) %>%
+    dplyr::mutate(
+      summ = file.path( outdir, "filter_fastq_summary",
+        stringr::str_c(name, "_trim_summary.rds")) %>%
         furrr::future_map(readRDS)) %>%
     tidyr::unnest() %>%
-    select(-fold_change)
+    dplyr::select(-fold_change)
 
   input_files %<>% select(-R1,-R2) %>%
     mutate(
@@ -45,19 +48,18 @@ summarize_number_reads <- function(reads_file,outprefix,outdir,cores)
 
   asv_table <- readRDS(file.path(outdir,"ASV_tables",paste0(outprefix,"_sequence_table.rds")))
 
-  asv_table %<>%
-    {
-      rs = rowSums(.)
-      tibble(
-        name = names(rs),
-        reads.asv_table = rs)}
+  asv_table %<>% {
+    rs <- rowSums(.)
+    tibble::tibble(
+      name = names(rs),
+      reads.asv_table = rs)}
 
   input_files %<>% inner_join(asv_table,by = "name")
   input_files %>%
     mutate(
       perc.out = reads.out / reads.in,
       perc.merged_pairs = reads.merged_pairs / reads.in,
-      perc.asv_table = reads.asv_table / reads.in  )
+      perc.asv_table = reads.asv_table / reads.in )
 
 }
 
@@ -121,6 +123,3 @@ plot_abundance_per_step <- function(nreads_summary, summary_fun = median, relati
 
 
 }
-
-
-
--- R/shared_functions.R
+++ R/shared_functions.R
@@ -1,102 +1,111 @@
-##' @import magrittr
-##' @import stringr
-##' @import dplyr
-##' @import readr
-##' @import optparse
-##' @import dada2
-##' @import DECIPHER
-##' @import furrr
-##' @import abind
+#' @import magrittr
+#' @import stringr
+#' @import dplyr
+#' @import readr
+#' @import optparse
+#' @import dada2
+#' @import DECIPHER
+#' @import furrr
+#' @import abind
 NULL
 
-
-##' converts from input files into the filtered file format for dada2 pipeline
-##'
-##' @param fastq a character vector with the name of a file
-##' @param outdir a character with the name of the output directory
-##'
-##' @return a character vector with modified
-##' @export
-parse_filtered_file <- function(fastq,outdir){
+#' converts from input files into the filtered file format for dada2 pipeline
+#'
+#' @param fastq a character vector with the name of a file
+#' @param outdir a character with the name of the output directory
+#'
+#' @return a character vector with modified
+#' @export
+parse_filtered_file <- function(fastq, outdir) {
 
   file.path(outdir,
-            stringr::str_replace(basename(fastq), ".fastq", "_filtered.fastq.gz"))
+    stringr::str_replace(
+      basename(fastq), ".fastq", "_filtered.fastq.gz"))
 
 }
 
+#' write csv files structured to make the dada2 pipeline
+#' run with condor
+#'
+#' @param all_samples a \code{tibble} with 3 columns:
+#'    name, end1, end2
+#' @param outfile the file where the full tibble is going to be saved,
+#'   the end1 and end2 files, are going to share almost the same name
+#'   but replacing `.csv` with `_R1.csv` and `_R2.csv`, respectively.
+#' @export
+save_files <- function(all_samples, outfile) {
 
-##' write csv files structured to make the dada2 pipeline
-##' run with condor
-##'
-##' @param all_samples a \code{tibble} with 3 columns:
-##'    name, R1, R2
-##' @param outfile the file where the full tibble is going to be saved,
-##'   the R1 and R2 files, are going to share almost the same name
-##'   but replacing `.csv` with `_R1.csv` and `_R2.csv`, respectively.
-##' @export
-save_files <- function(all_samples, outfile )
-{
-  R1 <- R2 <- NULL
-
-  all_samples %>% write_csv( outfile, col_names = FALSE)
-  all_samples %>% select(R1) %>%
-    write_csv(str_replace(outfile,".csv","_R1.csv"), col_names = FALSE)
-  all_samples %>% select(R2) %>%
-    write_csv(str_replace(outfile,".csv","_R2.csv"), col_names = FALSE)
-
+  end1 <- end2 <- NULL
+  all_samples %>% readr::write_csv(outfile, col_names = FALSE)
+  all_samples %>%
+    dplyr::select(end1) %>%
+    readr::write_csv(
+      stringr::str_replace(outfile, ".csv", "_R1.csv"), col_names = FALSE)
+  all_samples %>%
+    dplyr::select(end2) %>%
+    readr::write_csv(
+      stringr::str_replace(outfile, ".csv", "_R2.csv"), col_names = FALSE)
 }
 
-warning_file <- function(file,warn_text)
-{
-  if(!file.exists(file)){
-    if(file == ""){
+#' creates a warning if the file doesn't exists
+#' @param file a string with the name of the file
+#' @param warn_text a string with the text to be added as warning
+#' @export
+warning_file <- function(file, warn_text) {
+  if (!file.exists(file)) {
+    if (file == "") {
       warning(warn_text)
     }else{
-      warning(file," doesn't exists")
+      warning(file, " doesn't exists")
     }
   }
 }
 
-##' creates the file structure for running dada2 with condor
-##' @param outdir Name of the base output directory
-##' @export
-create_file_structure <- function(outdir)
-{
+#' creates the file structure for running dada2 with condor
+#' @param outdir Name of the base output directory
+#' @export
+create_file_structure <- function(outdir) {
   dir.create(outdir, showWarnings = FALSE)
 
   ### condor stuff
+  dir.create(file.path(outdir, "err"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "log"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "out"), showWarnings = FALSE)
 
-  dir.create(file.path(outdir,"err"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"log"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"out"), showWarnings = FALSE)
-
-  dir.create(file.path(outdir,"summary"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"filter_fastq"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"filter_fastq_summary"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"error_rates"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"merged_pairs"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"merged_pairs_summary"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"ASV_tables"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"ASV_tables","idtaxa"), showWarnings = FALSE)
-  dir.create(file.path(outdir,"ASV_tables", "prevalence"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "summary"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "filter_fastq"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "filter_fastq_summary"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "error_rates"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "merged_pairs"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "merged_pairs_summary"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "ASV_tables"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "ASV_tables", "idtaxa"), showWarnings = FALSE)
+  dir.create(file.path(outdir, "ASV_tables", "prevalence"),
+    showWarnings = FALSE)
 
 }
 
-##' generates all condor files
-##' @param condordir directory where all condor files are going to be saved
-##' @param prefix prefix to be used in all files
-##' @export
-condor_generate_all <- function(condordir, prefix)
-{
-  my_date <- Sys.Date()
-  my_date <- stringr::str_replace_all(my_date,"-","_")
-  prefix <- file.path(condordir,str_c(prefix,"_dada2"))
+#' generates all condor files
+#' @param condordir directory where all condor files are going to be saved
+#' @param prefix prefix to be used in all files
+#' @export
+condor_generate_all <- function(condordir, prefix) {
 
-  condor_filter_trim(condor_file = str_c(prefix,"_filter_and_trim_",my_date))
-  condor_error_rates(condor_file = str_c(prefix,"_learned_error_rates_",my_date))
-  condor_merge_pairs(condor_file = str_c(prefix,"_merge_pairs_",my_date))
-  condor_make_sequence_table(condor_file = str_c(prefix,"_make_seqtab_",my_date))
-  condor_remove_chimeras(condor_file = str_c(prefix,"_remove_chimeras_",my_date))
-  condor_label_taxa(condor_file = str_c(prefix,"_label_taxa_",my_date))
+  my_date <- Sys.Date()
+  my_date <- stringr::str_replace_all(my_date, "-", "_")
+  prefix <- file.path(condordir, stringr::str_c(prefix, "_dada2"))
+
+  condor_filter_trim(
+    condor_file = stringr::str_c(prefix, "_filter_and_trim_", my_date))
+  condor_error_rates(
+    condor_file = stringr::str_c(prefix, "_learned_error_rates_", my_date))
+  condor_merge_pairs(
+    condor_file = stringr::str_c(prefix, "_merge_pairs_", my_date))
+  condor_make_sequence_table(
+    condor_file = stringr::str_c(prefix, "_make_seqtab_", my_date))
+  condor_remove_chimeras(
+   condor_file = stringr::str_c(prefix, "_remove_chimeras_", my_date))
+  condor_label_taxa(
+    condor_file = stringr::str_c(prefix, "_label_taxa_", my_date))
 
 }
